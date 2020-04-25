94d75fe98d393a03d80e5736ef30c3962f07da8c
Shunsuke Hioki <shunsuke_hioki@fringe81.com>
2020-04-25 23:00:20 +0900

    timerでのstateの変更

--- src/App.js
+++ src/App.js
@@ -3,17 +3,35 @@ import Rect from './Rect';
 import './App.css';
 
 class App extends Component {
-  // constructor(props){
-  //   super();
-  //   this.title=props.title;
-  //   this.message=props.message;
-  // }
+  msgStyle = {
+    fontSize:"24pt",
+    color:"#900",
+    margin:"20px 0px",
+    padding:"5px",
+    borderBottom:"2px solid #900"
+  }
+  btnStyle={
+    fontSize:"20pt",
+    padding:"0px 10px"
+  }
+
+  constructor(props){
+    super(props);
+    this.state={
+      msg:'Hello'
+    };
+
+    let timer = setInterval(()=>{
+      this.setState((state)=>({
+        msg: state.msg + "!"
+      }));
+    },10000);
+  }
   render(){
     return <div>
       <h1>React</h1>
-      <Rect x="50" y="50" w="150" h="150" c="cyan" r="50"/>
-      <Rect x="150" y="100" w="150" h="150" c="magenta" r="75" />
-      <Rect x="100" y="150" w="150" h="150" c="black" r="25"/>
+      <p style={this.msgStyle}>{this.state.msg}</p>
+      <p style={this.msgStyle}>{this.props.msg}</p>
     </div>;
   }
 }
