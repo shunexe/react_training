c96f2e0ad1966294677596f50ff6d6787f63ebf4
Shunsuke Hioki <shunsuke_hioki@fringe81.com>
2020-04-25 22:36:39 +0900

    3-2 まで  複数コンポーネントの利用

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Rect from './Rect';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor(props){
+  //   super();
+  //   this.title=props.title;
+  //   this.message=props.message;
+  // }
+  render(){
+    return <div>
+      <h1>React</h1>
+      <Rect x="50" y="50" w="150" h="150" c="cyan" r="50"/>
+      <Rect x="150" y="100" w="150" h="150" c="magenta" r="75" />
+      <Rect x="100" y="150" w="150" h="150" c="black" r="25"/>
+    </div>;
+  }
 }
 
 export default App;
