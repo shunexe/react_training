b7395ed2ccfb41765b4315a468b82037888be44e
Shunsuke Hioki <shunsuke_hioki@fringe81.com>
2020-04-26 17:29:18 +0900

    オリジナルイベント作成

--- src/App.js
+++ src/App.js
@@ -18,34 +18,43 @@ class App extends Component {
     this.state={
       message:'type your name:'
     };
-    this.doChange = this.doChange.bind(this);
-    this.doSubmit = this.doSubmit.bind(this);
+    this.doCheck=this.doCheck.bind(this);
+  }
+  doCheck(event){
+    alert(event.target.value+"は長すぎます。（最大10文字)");
   }
 
-  doChange(event){
-    this.input = event.target.value;
-  }
-
-  doSubmit(event){
-    this.setState({
-      message:'Hello,'+ this.input + '!!'
-    });
-    event.preventDefault();
-  }
   render(){
     return <div>
       <h1>React</h1>
       <h2>{this.state.message}</h2>
-      <form onSubmit={this.doSubmit}>
-        <label>
-          <span style={this.inputStyle}></span>Message:
-          <input type="text" style={this.inputStyle} onChange={this.doChange} required pattern="[A-Za-z _,.]+"/>
-        </label>
-        <input type="submit" style={this.inputStyle} value="Click" />
-      </form>
+      <Message maxlength="10" onCheck={this.doCheck} />
     </div>;
   }
 }
 
+class Message extends Component{
+  inputStyle={
+    fontSize:"12pt",
+    padding:"5px"
+  }
+
+  constructor(props){
+    super(props);
+    this.doChange=this.doChange.bind(this);
+  }
+
+  doChange(e){
+    if(e.target.value.length > this.props.maxlength){
+      this.props.onCheck(e);
+      e.target.value=
+        e.target.value.substr(0,this.props.maxlength);
+    }
+  }
+
+  render(){
+    return <input type="text" style={this.inputStyle} onChange={this.doChange} />
+  }
+}
 
 export default App;
