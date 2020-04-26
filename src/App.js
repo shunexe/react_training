07e19d76f8213dfba600001107dafaa16a6e2be8
Shunsuke Hioki <shunsuke_hioki@fringe81.com>
2020-04-26 11:07:37 +0900

    stateに配列を保存してレンダリング

--- src/App.js
+++ src/App.js
@@ -1,55 +1,58 @@
 import React,{Component} from 'react';
 import Rect from './Rect';
 import './App.css';
+import _ from 'lodash'
 
 class App extends Component {
-  msgStyle1 = {
-    fontSize:"24pt",
+  data =[];
+  msgStyle = {
+    fontSize:"20pt",
     color:"#900",
     margin:"20px 0px",
     padding:"5px",
-    borderBottom:"2px solid #900"
   }
-
-  msgStyle2 = {
-    fontSize:"20pt",
-    color:"white",
-    backgroundColor:"#900",
-    margin:"20px 0px",
-    padding:"5px",
-    borderBottom:"2px solid #900"
-  }
-
-  btnStyle={
-    fontSize:"16pt",
-    padding:"10px"
+  area ={
+    width:"500px",
+    height:"500px",
+    border:"1px solid blue"
   }
 
   constructor(props){
     super(props);
     this.state={
-      counter:0,
-      msg:'count start!!!',
-      flg:true
+      list:this.data
     };
     this.doAction=this.doAction.bind(this);
   }
   doAction(e){
-    this.setState((state)=>({
-      counter:state.counter + 1,
-      msg:state.counter,
-      flg: !state.flg
+    let x=e.pageX;
+    let y=e.pageY;
+    let newData = _.cloneDeep(this.state.list)
+    newData.push({x:x,y:y});//参照渡しになってる→値渡し　
+    this.setState(({
+      list:newData
     }));
   }
+
+  draw(d){
+    let s={
+      position:"absolute",
+      left:(d.x-25)+"px",
+      top:(d.y-25)+"px",
+      width:"50px",
+      height:"50px",
+      backgroundColor:"#66f3"
+    };
+    return <div style={s}></div>;
+  }
+
   render(){
     return <div>
       <h1>React</h1>
-      {this.state.flg ? 
-      <p style={this.msgStyle1}>{this.state.msg}</p>
-      :
-      <p style={this.msgStyle2}>{this.state.msg}です。</p>
-    }
-      <button style={this.btnStyle} onClick={this.doAction}>Click</button>
+      <h2 style={this.msgStyle}>show rect.</h2>
+      <div style={this.area} onClick={this.doAction}>
+        {this.state.list.map((value)=>this.draw(value))}
+      </div>
     </div>;
   }
 }
