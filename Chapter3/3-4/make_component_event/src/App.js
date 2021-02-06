b834d21b7872c2483d88e58feb83c6a94e28cb0e
Shunsuke Hioki <shunsuke_hioki@fringe81.com>
2021-02-06 17:07:07 +0900

    コンポーネント独自のイベント作成

---
+++ Chapter3/3-4/make_component_event/src/App.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react'
+import './App.css';
+
+class App extends Component {
+    input =''
+
+    msgStyle={
+        fontSize:"20pt",
+        color:"#900",
+        margin:"20px 0px",
+        padding: "5px",
+    }
+
+    constructor(props) {
+        super(props);
+        this.state={
+            message:'type your name:'
+        };
+        this.doCheck= this.doCheck.bind(this);
+    }
+
+    doCheck(event){
+        alert(event.target.value +
+            "は長すぎます。(最大10文字)"
+        );
+    }
+
+    render(){
+        return <div>
+            <h1>React</h1>
+            <h2>{this.state.message}</h2>
+            <Message maxlength="10" onCheck={this.doCheck}/>
+        </div>;
+    }
+}
+
+class Message extends Component{
+    inputStyle={
+        fontSize: "12pt",
+        padding:"5px"
+    }
+
+    constructor(props) {
+        super(props);
+        this.doChange = this.doChange.bind(this);
+    }
+
+    doChange(e){
+        if(e.target.value.length >this.props.maxlength){
+            this.props.onCheck(e);
+            //↓入力値から上限を越えた文字を削ってinputタグに反映している。
+            e.target.value =
+                e.target.value.substr(0,this.props.maxlength);
+        }
+    }
+
+    render(){
+        return <input type="text" style={this.inputStyle}
+            onChange = {this.doChange}/>
+    }
+}
+
+export default App;
